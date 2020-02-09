e32224a2c925929e9079671edde63de515ef6eb8
Zaman <Sunny.Zaman@ontario.ca>
2020-02-08 21:08:32 -0500

    Connected to database

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    include "database.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google" content="notranslate" />
+    <meta http-equiv="Content-Language" content="en_US" /> 
+    <title>Student Records</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+<body>
+    
+    <div class="container">
+        <h1 class="title m-t10">Student Records</h1>
+        
+    </div>
+</body>
+
+</html>
