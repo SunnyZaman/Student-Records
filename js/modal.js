8f5c4a4e9c14c2397751f24f7070fa6e5a8293e8
Zaman <Sunny.Zaman@ontario.ca>
2020-02-09 22:36:03 -0500

    Disable submit button on Cancel

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,8 @@
 $(".modal-open").click(function() {
-    $(".modal").addClass("is-active");  
-  });
-  
-  $(".modal-close, .close").click(function() {
-     $(".modal").removeClass("is-active");
-  });
+   $(".modal").addClass("is-active");  
+ });
+ 
+ $(".modal-close, .close").click(function() {
+    $(".modal").removeClass("is-active");
+       $('.submit-button').attr('disabled', 'disabled');  
+ });
